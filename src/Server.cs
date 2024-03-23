689427874eb2a8755a92d370ebc949630ec7b3f4
Leandro Gabriel Casas <leandrogabriel.casas@gmail.com>
2024-03-23 18:02:53 -0300

    3rd stage

--- src/Server.cs
+++ src/Server.cs
@@ -2,14 +2,37 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-//Console.WriteLine("Logs from your program will appear here!");
-
 var server = new TcpListener(IPAddress.Any, 4221);
 
-server.Start();
+server.Start(); //listen for client requests
 
-var client = server.AcceptTcpClient(); // return a reference to the tcpclient to send and receive
-var outputBytes = Encoding.UTF8.GetBytes($"HTTP/1.1 200 OK\r\n\r\n"); // dencode the string in UT F8
+byte[] bytes = new byte[1024];// Buffer for reading data
+string data;
 
-client.GetStream().Write(outputBytes); // get the network stream and write bytes to it
+Console.WriteLine("Waiting for a connection... ");
+
+var client = server.AcceptTcpClient(); // blocking call to return a reference to the tcpclient to send and receive
+
+Console.WriteLine("Connected!");
+
+var networkStream = client.GetStream(); //get the network stream
+
+int bytesRead = networkStream.Read(bytes, 0, bytes.Length);// Receive all the data sent by the client.
+
+data = Encoding.ASCII.GetString(bytes.AsSpan(0, bytesRead));// Translate data bytes to a ASCII string
+
+// process
+var lines = data.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+var startLine = lines[1];
+
+var responseBuilder = new StringBuilder("HTTP/1.1 ");
+if (startLine == "/")
+    responseBuilder.Append("200 OK\r\n\r\n");
+else
+    responseBuilder.Append("404 Not Found\r\n\r\n");
+
+byte[] msg = Encoding.ASCII.GetBytes(responseBuilder.ToString());
+
+networkStream.Write(msg);// Write buffer into the network stream
+
+client.Close();// dispose tcp client and request tcp connection to close.
